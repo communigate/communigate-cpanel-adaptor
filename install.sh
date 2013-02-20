06b72f0509f5d1f1b406076d9a55fda90fa3f901
Anton Katsarov <anton@katsarov.org>
2013-02-20 11:54:00 +0200

    * Update Icon set * Installing new classes yaml by default in install script

--- install.sh
+++ install.sh
@@ -173,6 +173,7 @@ done
 # Update Feature List
 cp ${PACKSRC}/featurelists/cgpro /usr/local/cpanel/whostmgr/addonfeatures/
 chmod +x ${PACKSRC}/scripts/*
+${PACKSRC}/scripts/migrate_groupware.pl
 ${PACKSRC}/scripts/modify_features.pl
 /usr/local/cpanel/bin/rebuild_sprites
 /usr/local/cpanel/bin/build_locale_databases
