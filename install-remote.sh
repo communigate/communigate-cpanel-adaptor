fb6949690a890db655e39f60518928013b621951
Anton Katsarov <anton@katsarov.org>
2013-03-06 17:46:08 +0200

    * Modified install script for remote CGPro

---
+++ install-remote.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+PACKSRC=`pwd`
+source ${PACKSRC}/config.ini
+
+#################################################
+#               CommuniGate Specific  	 	#
+#################################################
+
+# Stop some services
+service exim stop
+service dovecot stop
+chkconfig exim off
+chkconfig dovecot off
+service cpanel stop
+service httpd stop
+
+#################################################
+#		cPanel Specific			#
+#################################################
+
+# iPhone provisioning using default httpd
+mkdir -p /usr/local/apache/htdocs/iOS
+chmod 777 /usr/local/apache/htdocs/iOS
+mkdir -p /var/CommuniGate/apple
+cp ${PACKSRC}/iphone/iphonetemplate.mobileconfig /var/CommuniGate/apple/
+
+# Install the WHM plugins
+cp -rf ${PACKSRC}/whm/cgi/* /usr/local/cpanel/whostmgr/docroot/cgi/
+cp ${PACKSRC}/whm/templates/* /usr/local/cpanel/whostmgr/docroot/templates/
+
+# Install CGP Logo
+cp ${PACKSRC}/whm/communigate.gif /usr/local/cpanel/whostmgr/docroot/images/communigate.gif
+
+# Install cPanel CommuniGate Custom Module
+cp ${PACKSRC}/module/CommuniGate.pm /usr/local/cpanel/Cpanel/
+
+# Lets add CGPro perl lib
+cp ${PACKSRC}/library/CLI.pm /usr/local/cpanel/perl/
+ln -s /usr/local/cpanel/perl/CLI.pm /usr/local/lib/perl5/5.8.8/
+
+# CGPro cPanel Wrapper
+cp ${PACKSRC}/cpwrap/ccaadmin /usr/local/cpanel/bin/
+cp ${PACKSRC}/cpwrap/ccawrap /usr/local/cpanel/bin/
+
+# Install cPanel Function hooks
+if [ ! -d /var/cpanel/perl5/lib/ ]
+then
+    mkdir -p /var/cpanel/perl5/lib/
+fi
+cp -rf ${PACKSRC}/hooks/CGPro /var/cpanel/perl5/lib/
+# Register installed hooks
+/usr/local/cpanel/bin/manage_hooks add module CGPro::Hooks
+
+#Install config file
+cp ${PACKSRC}/etc/cpanel_cgpro.conf /var/cpanel/communigate.yaml
+
+chmod 600 /var/cpanel/communigate.yaml
+
+# Install CommuniGate Webmail in cPanel
+cp ${PACKSRC}/cgpro-webmail/webmail_communigate.yaml /var/cpanel/webmail/
+cp -r ${PACKSRC}/cgpro-webmail/CommuniGate /usr/local/cpanel/base/3rdparty/
+
+# Install SSO for Webmail
+mkdir -p /var/CommuniGate/cgi
+cp ${PACKSRC}/sso/login.pl /var/CommuniGate/cgi/
+
+# chkservd for CGServer & spamd
+cp ${PACKSRC}/chkservd/CommuniGate /etc/chkserv.d/
+cp ${PACKSRC}/chkservd/CommuniGate_spamd /etc/chkserv.d/
+echo "CommuniGate:1" >> /etc/chkserv.d/chkservd.conf
+echo "CommuniGate_spamd:1" >> /etc/chkserv.d/chkservd.conf
+
+# Check the scripts have executable flag
+chmod +x /usr/local/cpanel/whostmgr/docroot/cgi/addon_cgpro*
+chmod +x /var/CommuniGate/cgi/login.pl
+chmod +x /usr/local/cpanel/Cpanel/CommuniGate.pm
+chmod +x /usr/local/cpanel/bin/ccaadmin
+chmod +s+x /usr/local/cpanel/bin/ccawrap
+chmod u+s /opt/CommuniGate/mail
+
+# Install CommuniGate Plugin
+BASEDIR='/usr/local/cpanel/base/frontend';
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+THEMES=($(find ${BASEDIR} -maxdepth 1 -mindepth 1 -type d))
+IFS=$OLDIFS
+
+tLen=${#THEMES[@]}
+
+LOCALES=($(find ${PACKSRC}/locale -maxdepth 1 -mindepth 1))
+lLen=${#LOCALES[@]}
+
+for (( i=0; i<${tLen}; i++ ));
+do
+    if [ "${THEMES[$i]}" == "${BASEDIR}/CommuniGate" ]
+    then
+        continue
+    fi
+    cp -r "${PACKSRC}/theme/cgpro" "${THEMES[$i]}/"
+    cp "${PACKSRC}/icons/"* "${THEMES[$i]}/branding"
+    cp "${PACKSRC}/plugin/dynamicui_cgpro.conf" "${THEMES[$i]}/dynamicui/"
+    if [ ! -d ${THEMES[$i]}/js2-min/cgpro ]
+    then
+	mkdir -p ${THEMES[$i]}/js2-min/cgpro
+    fi
+    if [ ! -L ${THEMES[$i]}/js2-min/cgpro/mail ]
+    then
+	ln -s ${THEMES[$i]}/js2-min/mail ${THEMES[$i]}/js2-min/cgpro/
+    fi
+    if [ ! -d ${THEMES[$i]}/css2-min/cgpro ]
+    then
+	mkdir -p ${THEMES[$i]}/css2-min/cgpro
+    fi
+    if [ ! -L ${THEMES[$i]}/css2-min/cgpro/mail ]
+    then
+	ln -s ${THEMES[$i]}/css2-min/mail ${THEMES[$i]}/css2-min/cgpro/
+    fi
+    chmod +x ${THEMES[$i]}/cgpro/backup/getaccbackup.live.cgi
+    chmod +x ${THEMES[$i]}/cgpro/backup/getaliasesbackup.live.cgi
+    chmod +x ${THEMES[$i]}/cgpro/backup/getfiltersbackup.live.cgi
+    for ((j=0; j<${lLen}; j++)); do
+        TARGET=${THEMES[$i]}/locale/`basename ${LOCALES[$j]} '{}'`.yaml.local
+        if [ ! -f ${TARGET} ]
+        then
+            echo "---" > ${TARGET}
+        else
+            sed -i -e '/^CGP/d' ${TARGET}
+        fi
+        cat ${LOCALES[$j]} >> ${TARGET}
+    done
+done
+# Update Feature List
+cp ${PACKSRC}/featurelists/cgpro /usr/local/cpanel/whostmgr/addonfeatures/
+chmod +x ${PACKSRC}/scripts/*
+${PACKSRC}/scripts/migrate_groupware.pl
+${PACKSRC}/scripts/modify_features.pl
+/usr/local/cpanel/bin/rebuild_sprites
+/usr/local/cpanel/bin/build_locale_databases
+${PACKSRC}/scripts/editconfig.pl
+
+#################################################
+#             	  OS Specific	  	 	#
+#################################################
+
+# Firewall opening for CommuniGate Ports
+iptables -I INPUT -p tcp -d `hostname` --dport 8010 -j ACCEPT
+iptables -I INPUT -p tcp -d `hostname` --dport 9010 -j ACCEPT
+iptables -I INPUT -p tcp -d `hostname` --dport 8100 -j ACCEPT
+iptables -I INPUT -p tcp -d `hostname` --dport 9100 -j ACCEPT
+iptables-save
+
+# Start needed services
+service	cpanel start
+service	httpd start
+
+# Installing iTool Labs webmail
+sh ${PACKSRC}/webmail-install.sh
+
+echo "Dont forget to run disable-services.pl script to stop cPanel's native mail software"
---
+++ scripts/editconfig.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib '/usr/local/cpanel';
+use Cpanel::CachedDataStore ();
+
+my $correct = "n";
+my $data = Cpanel::CachedDataStore::fetch_ref( '/var/cpanel/communigate.yaml' ) || {};
+my $newdata = $data;
+my $cgprohost = $data->{cgprohost};
+my $cgproport = $data->{cgproport};
+my $cgprouser = $data->{cgprouser};
+my $cgpropass = $data->{cgpropass};
+do {
+  # Enter Hostname
+  print "Enter CommuniGate Pro IP address: [" . $cgprohost . "]: ";
+  $cgprohost = <>;
+  chomp $cgprohost;
+  $cgprohost = $newdata->{cgprohost} unless $cgprohost;
+  # Enter port
+  print "Enter CommuniGate Pro Port: [" . $cgproport . "]: ";
+  $cgproport = <>;
+  chomp $cgproport;
+  $cgproport = $newdata->{cgproport} unless $cgproport;
+  # Enter user
+  print "Enter CommuniGate Pro Username: [" . $cgprouser . "]: ";
+  $cgprouser = <>;
+  chomp $cgprouser;
+  $cgprouser = $newdata->{cgprouser} unless $cgprouser;
+  my $changepass = 'y';
+  do {
+    print "Change CommuniGate Pro password? [Y/n]: ";
+    $changepass = <>;
+    chomp $changepass;
+    $changepass = 'y' unless $changepass;
+    print "Please answer 'y' or 'n'.\n" unless $changepass =~ m/[yn]/i;
+  } while ($changepass !~ m/^[yn]$/i);
+
+  if ( $changepass =~ m/^y$/i ) {
+    # Enter pass
+    system('stty', '-echo');	# Disable echo
+    my $repass = "";
+    do {
+      print "Enter CommuniGate Pro Password: ";
+      $cgpropass = <>;
+      chomp $cgpropass;
+      print "\n";
+      print "Enter CommuniGate Pro Password again: ";
+      $repass = <>;
+      chomp $repass;
+      if ($cgpropass ne $repass) {
+        print "\n";
+        print "Passwords do not match! \n";
+      } else {
+        print "\n";
+        print "Password must be at least 4 symbols! \n" unless $repass =~ m/\S{4}/;
+      }
+      print "\n";
+    } while (($cgpropass ne $repass) || $repass !~ m/\S{4}/);
+    system('stty', 'echo');	# Turn echo back on
+  }
+  do {
+    print "==- Verify Data -==================== \n";
+    print "CommuniGate Pro IP address: $cgprohost \n";
+    print "CommuniGate Pro Port: $cgproport \n";
+    print "CommuniGate Pro Username: $cgprouser \n";
+    print "===================================== \n";
+    print "Is all data correct? [Y/n]: ";
+    $correct = <>;
+    chomp $correct;
+    $correct = 'y' unless $correct;
+    print "Please answer 'y' or 'n'.\n" unless $correct =~ m/[yn]/i;
+  } while ($correct !~ m/^[yn]$/i);
+  $newdata->{cgprouser} = $cgprouser;
+  $newdata->{cgpropass} = $cgpropass;
+  $newdata->{cgprohost} = $cgprohost;
+  $newdata->{cgproport} = $cgproport;
+} while ($correct !~ m/^y$/i);
+Cpanel::CachedDataStore::store_ref( '/var/cpanel/communigate.yaml', $newdata );
