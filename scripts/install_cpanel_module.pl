a6af435826525ad1f65c693a6cec6ca2b8ebffb3
Anton Katsarov <anton@katsarov.org>
2013-08-16 18:42:21 +0300

    * Bugfix

--- scripts/install_cpanel_module.pl
+++ scripts/install_cpanel_module.pl
@@ -2,7 +2,7 @@
 
 use CPAN;
 exit unless $ARGV[0];
-my $missing = system("/usr/local/cpanel/3rdparty/bin/perl -MMIME::QuotedPrint::Perl -e 1 2>/dev/null");
+my $missing = system("/usr/local/cpanel/3rdparty/bin/perl -M$ARGV[0] -e 1 2>/dev/null");
 if ($missing) {
     CPAN::Shell->force("install",$ARGV[0]);
 }
